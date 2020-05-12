439a40a9502a37c5d152d35ea65ce3a98bc4b607
Fanny Ahlm <fannyahlm@fannys-mbp.mynet>
2020-05-12 03:26:41 +0200

    Updates

---
+++ precache-manifest.e44fe78aac87288914bac34536b5ff1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46c3647e5769a353e9a6afce3045d85",
+    "url": "/TheStarWarsProject/index.html"
+  },
+  {
+    "revision": "3b1175daee05ec5d2310",
+    "url": "/TheStarWarsProject/static/css/main.e1bcce07.chunk.css"
+  },
+  {
+    "revision": "02e8b07c04e930d73bea",
+    "url": "/TheStarWarsProject/static/js/2.468e095f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TheStarWarsProject/static/js/2.468e095f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1175daee05ec5d2310",
+    "url": "/TheStarWarsProject/static/js/main.09da850d.chunk.js"
+  },
+  {
+    "revision": "b0c556bd35c13a2dc2ca",
+    "url": "/TheStarWarsProject/static/js/runtime-main.6ca10ef1.js"
+  },
+  {
+    "revision": "9f22547098ad19708fa998709b7d881c",
+    "url": "/TheStarWarsProject/static/media/bakgrund2.9f225470.jpeg"
+  }
+]);
